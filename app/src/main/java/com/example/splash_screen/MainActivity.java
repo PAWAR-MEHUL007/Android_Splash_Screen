e33757fda75d7c687e525a9b3c189f76cea83c08
PAWAR-MEHUL007 <mpavar007@gmail.com>
2021-09-30 12:48:30 -0600

    Initial commit

---
+++ app/src/main/java/com/example/splash_screen/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.splash_screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.util.Log;
+
+
+import com.google.android.material.tabs.TabLayout;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        loadFragment(new FragmentOne());
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Log.d("tabs","" + tab.getPosition());
+                if(tab.getPosition()==0)
+                {
+                    loadFragment(new FragmentOne());
+                }
+                else if(tab.getPosition()==1)
+                {
+                    loadFragment(new FragmentTwo());
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+    private void loadFragment(Fragment fragment)
+    {
+        FragmentManager fm =getFragmentManager();
+        final FragmentTransaction ft =fm.beginTransaction();
+        ft.replace(R.id.frmLayout, fragment);
+        ft.commit();
+
+    }
+}
---
+++ app/src/main/java/com/example/splash_screen/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.splash_screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+//import java.lang.Runnable;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            Intent i=new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(i);
+        },5000);
+    }
+}
